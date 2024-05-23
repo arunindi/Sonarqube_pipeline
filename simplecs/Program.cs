c0f9049ce2fa987f9d8debe84a74a08196985dc1
arunindi <125126755+arunindi@users.noreply.github.com>
2024-05-23 16:31:58 +0530

    Update Program.cs

--- simplecs/Program.cs
+++ simplecs/Program.cs
@@ -1,13 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-while (true)
-        {
-            Console.WriteLine("Inside the loop. Press Ctrl+C to exit.");
-            // Add some logic here
-            
-            // Optional: Add a delay to prevent high CPU usage
-            // System.Threading.Thread.Sleep(1000); // Sleep for 1 second
-        }
+
     private static void UnusedMethod() 
     {
         Console.WriteLine("Unused method.");
