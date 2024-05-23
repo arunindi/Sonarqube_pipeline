28d56e03104cf45c6f999ea931b7c13f9d4456b2
arunindi <125126755+arunindi@users.noreply.github.com>
2024-05-23 16:37:57 +0530

    Update Program.cs

--- simplecs/Program.cs
+++ simplecs/Program.cs
@@ -1,14 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-    private static void UnusedMethod() 
-    {
-        Console.WriteLine("Unused method.");
-    }
-
-    // Duplicate code
-    private static void PrintMessage() 
-    {
-        Console.WriteLine("Duplicate line.");
-        Console.WriteLine("Duplicate line.");
-    }
+private static void PrintMessage() 
+{
+    Console.WriteLine("Duplicate line.");
+    Console.WriteLine("Duplicate line.");
+}
